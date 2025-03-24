69980b5b53ce80fc64ceb4933fa7868769af2dc7
Apurav <apurav0711@gmail.com>
2025-03-24 11:41:45 +0530

    modified:   project/app/api/messages/[sender]/[receiver]/route.js

--- project/app/api/messages/[sender]/[receiver]/route.js
+++ project/app/api/messages/[sender]/[receiver]/route.js
@@ -10,6 +10,7 @@ export async function GET(req, { params }) {
 
         await connectDB();
         const messages = await Message.find({ receiver: receiver, sender: sender });
+        const msg2 = await Message.find({ receiver: sender, sender: receiver });
         // console.log(messages);
         const data = messages.map((msg) => {
             return {
@@ -18,8 +19,22 @@ export async function GET(req, { params }) {
                 receiver: msg.receiver,
             }
         })
+
+        const data2 = msg2.map((msg) => {
+            return {
+                content: msg.content,
+                sender: msg.sender,
+                receiver: msg.receiver,
+            }
+        })
+
+        const combined_data = [
+            ...data,
+            ...data2,
+        ]
+        console.log(combined_data);
         // console.log(data);
-        return NextResponse.json(data, { status: 200 });
+        return NextResponse.json(combined_data, { status: 200 });
 
     } catch (e) {
         return NextResponse.json({ err: e.message }, { status: 500 });
